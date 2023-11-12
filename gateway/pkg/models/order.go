99845256dd3cd40bba4bf9d49322b5ae8023ace5
Alex Rokachev <alserokachev@gmail.com>
2023-11-12 17:07:49 +0300

    created order struct

--- gateway/pkg/models/order.go
+++ gateway/pkg/models/order.go
@@ -5,15 +5,10 @@ import "time"
 type CreateOrderReq struct {
 }
 
-type OrderDevice struct {
-	UserUUID           string
-	DeviceUUID         string
-	DeviceTitle        string
-	DeviceDescription  string
-	DevicePrice        float32
-	DeviceManufacturer string
-	OrderAmount        int32
-	OrderUUID          string
-	OrderStatus        string
-	OrderCreatedAt     *time.Time
+type Order struct {
+	UserUUID  string
+	OrderUUID string
+	Devices   []*Device
+	Status    string
+	CreatedAt time.Time
 }
