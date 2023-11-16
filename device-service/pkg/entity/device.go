722d556d6d6758b4f6a14db238222f4e89bca60c
Alex Rokachev <alserokachev@gmail.com>
2023-11-16 17:44:31 +0300

    removed invalid validate condition

--- device-service/pkg/entity/device.go
+++ device-service/pkg/entity/device.go
@@ -4,7 +4,7 @@ type Device struct {
 	UUID         string  `json:"uuid,omitempty" bson:"uuid"`
 	Title        string  `json:"title,omitempty" bson:"title" validate:"required,min=3"`
 	Description  string  `json:"description,omitempty" bson:"description"`
-	Price        float32 `json:"price,omitempty" bson:"price" validate:"required,gt=0.0~Price can't be less or equal than 0'"`
+	Price        float32 `json:"price,omitempty" bson:"price" validate:"required,gt=0.0"`
 	Manufacturer string  `json:"manufacturer,omitempty" bson:"manufacturer" validate:"required"`
 	Amount       uint32  `bson:"amount,omitempty" bson:"amount" validate:"gt=0"`
 }
