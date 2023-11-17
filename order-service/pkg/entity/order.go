7f0866dc579ebb2ffce031d2c5aaad8f754e9388
Alex Rokachev <alserokachev@gmail.com>
2023-11-17 22:08:05 +0300

    updated structs

--- order-service/pkg/entity/order.go
+++ order-service/pkg/entity/order.go
@@ -1,18 +1,9 @@
 package entity
 
 import (
-	"github.com/alserov/device-shop/device-service/pkg/entity"
 	pb "github.com/alserov/device-shop/proto/gen"
 	"time"
 )
-
-type Order struct {
-	UserUUID  string
-	OrderUUID string
-	Devices   []*entity.Device
-	Status    string
-	CreatedAt time.Time
-}
 
 type OrderedDevice struct {
 	Status     int32
@@ -22,18 +13,18 @@ type OrderedDevice struct {
 }
 
 type CreateOrderReq struct {
-	UserUUID    string `json:"userUUID,omitempty" validate:"required"`
-	OrderUUID   string `json:"orderUUID,omitempty" validate:"required"`
-	Status      int32  `json:"status,omitempty" validate:"required"`
-	CreatedAt   *time.Time
-	DeviceUUIDs []string `json:"devices,omitempty" validate:"min=1"`
+	UserUUID    string     `json:"userUUID,omitempty" validate:"required"`
+	OrderUUID   string     `json:"orderUUID,omitempty" validate:"required"`
+	Status      int32      `json:"status,omitempty" validate:"required"`
+	CreatedAt   *time.Time `json:"createdAt,omitempty"`
+	DeviceUUIDs []string   `json:"devices,omitempty" validate:"min=1"`
 }
 
 type CreateOrderReqWithDevices struct {
 	UserUUID  string `json:"userUUID,omitempty" validate:"required"`
-	OrderUUID string `json:"orderUUID,omitempty" validate:"required"`
-	Status    int32  `json:"status,omitempty" validate:"required"`
-	CreatedAt *time.Time
+	OrderUUID string
+	Status    int32
+	CreatedAt time.Time
 	Devices   []*pb.Device `json:"devices,omitempty" validate:"min=1"`
 }
 
